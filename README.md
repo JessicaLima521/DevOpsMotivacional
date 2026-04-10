75fdd3bf7f4c424c67ab46de231c5855fb44f126
JessicaLima521 <jessicaambroziodelima@gmail.com>
2026-04-10 20:57:53 -0300

    Testes finais

--- README.md
+++ README.md
@@ -14,3 +14,14 @@ e conceitos utilizados em pipelines de CI/CD.
 
 
 python app.py
+### 1. Build da imagem
+docker build -t devops-motivacional .
+
+
+### 2. Rodar o container
+docker run -p 3000:3000 devops-motivacional
+
+### 3. Acessar no navegador
+http://localhost:3000
+
+Aplicação testada e funcionando via Docker
--- test_frases.py
+++ test_frases.py
@@ -1,5 +1,23 @@
 from frases import mostrar_frase
 
 def test_frase():
+    
     frase = mostrar_frase()
+    
     assert frase != ""
+
+def test_tipo_string():
+    
+    assert isinstance(mostrar_frase(), str)
+
+def test_nao_vazia():
+    
+    assert mostrar_frase() != ""
+
+def test_tamanho():
+    
+    assert len(mostrar_frase()) > 3
+
+def test_nao_none():
+    
+    assert mostrar_frase() is not None
